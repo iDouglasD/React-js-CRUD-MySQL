cbaa44ce51fce42de75d0714286c721530179783
Douglas Duarte <douglasduarte.eng@gmail.com>
2022-01-28 07:39:03 -0300

    Form register and get data

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,53 @@
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
-    return <div className="App"></div>;
+    const [values, setValues] = useState();
+
+    const valuesChangeHandler = (value) => {
+        setValues((prevValues) => ({
+            ...prevValues,
+            [value.target.name]: value.target.value,
+        }));
+    };
+
+    const registerChangeHandler = () => {
+        console.log(values);
+    };
+    return (
+        <div className="app-container">
+            <div className="register-container">
+                <h1>Scrim Shop</h1>
+                <input
+                    type="text"
+                    name="name"
+                    placeholder="Nome"
+                    className="register-input"
+                    onChange={valuesChangeHandler}
+                />
+                <input
+                    type="text"
+                    name="cost"
+                    placeholder="Preço"
+                    className="register-input"
+                    onChange={valuesChangeHandler}
+                />
+                <input
+                    type="text"
+                    name="category"
+                    placeholder="Categoria"
+                    className="register-input"
+                    onChange={valuesChangeHandler}
+                />
+                <button
+                    className="register-button"
+                    onClick={() => registerChangeHandler()}
+                >
+                    Cadastrar
+                </button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
